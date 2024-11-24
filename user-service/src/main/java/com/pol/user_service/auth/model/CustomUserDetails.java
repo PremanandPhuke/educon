7108bb83a661ec36cd93e6cc0431a38d76609ec0
Vignesh Arumugam <avvickya@gmail.com>
2024-11-24 11:51:41 +0530

    add authurization

---
+++ user-service/src/main/java/com/pol/user_service/auth/model/CustomUserDetails.java
@@ -0,0 +1,9 @@
+package com.pol.user_service.auth.model;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface CustomUserDetails extends UserDetails {
+    UUID getId();
+}
